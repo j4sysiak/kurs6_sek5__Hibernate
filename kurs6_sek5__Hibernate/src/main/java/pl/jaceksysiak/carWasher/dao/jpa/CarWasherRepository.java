5270ecc4119382e3c8477932f1f7a930e129d8fb
Jacek <Jacek@BERLIN>
2020-04-18 13:39:45 +0200

    k

--- kurs6_sek5__Hibernate/src/main/java/pl/jaceksysiak/carWasher/dao/jpa/CarWasherRepository.java
+++ kurs6_sek5__Hibernate/src/main/java/pl/jaceksysiak/carWasher/dao/jpa/CarWasherRepository.java
@@ -15,18 +15,18 @@ import pl.jaceksysiak.carWasher.model.Car;
 
 
 @Repository
-@Transactional(rollbackFor = Exception.class)
+//@Transactional(rollbackFor = Exception.class)
 public class CarWasherRepository {
 
 	@Autowired
 	private EntityManager entityManager;
 
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	//@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<Car> getCars() {
 		return entityManager.createQuery("from Car", Car.class).getResultList();
 	}
 
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	//@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public Car getCar(int id) {
 		return entityManager.find(Car.class, id);
 	}
